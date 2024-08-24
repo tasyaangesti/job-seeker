7336554473093bc1d0796313d3ee92ba21dfa425
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-08-24 22:22:59 +0700

    feat: login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
+const { where } = require("sequelize");
 const { User } = require("../models");
+const { comparePassword } = require("../helper/bcrypt");
+const { signToken } = require("../helper/jwt");
 
 class UserController {
   static async register(req, res) {
@@ -17,6 +20,45 @@ class UserController {
         role: newUser.role,
         email: newUser.email,
       });
+    } catch (error) {
+      console.log(error);
+      if (
+        error.name === "SequelizeUniqueConstraintError" ||
+        error.name === "SequelizeValidationError"
+      ) {
+        res.status(400).json({ message: error.errors[0].message });
+      } else {
+        res.status(500).json({ message: "internal server error" });
+      }
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { code: 400, message: "Email is Required" };
+      }
+
+      if (!password) {
+        throw { code: 400, message: "Password is Required" };
+      }
+
+      const findUser = await User.findOne({ where: { email } });
+      if (!findUser) {
+        throw { code: 401, message: "Invalid Email or Password" };
+      }
+
+      const isPasswordSame = comparePassword(password, findUser.password);
+      if (!isPasswordSame) {
+        throw { code: 401, message: "Invalid Email or Password" };
+      }
+
+      const access_token = signToken({ id: findUser.id });
+
+      res
+        .status(200)
+        .json({ access_token, email: findUser.email, role: findUser.role });
     } catch (error) {
       console.log(error);
       if (
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const UserController = require("../controllers/userController");
 const user = express.Router();
 
 user.post("/register", UserController.register);
+user.post("/login", UserController.login);
 
 module.exports = user;
